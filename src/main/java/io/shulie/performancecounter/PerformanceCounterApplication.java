7a1e9782ea8c07d2b39469d6df326506f5c0ad11
陈星星 <chenxingxing@shulie.io>
2022-12-09 18:35:46 +0800

    Initial commit

---
+++ src/main/java/io/shulie/performancecounter/PerformanceCounterApplication.java
@@ -0,0 +1,13 @@
+package io.shulie.performancecounter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PerformanceCounterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PerformanceCounterApplication.class, args);
+    }
+
+}
